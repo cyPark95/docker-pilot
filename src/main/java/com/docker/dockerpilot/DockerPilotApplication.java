c0c0910ad5375d095d6aeb50dd6b3fabba0faaed
cyPark95 <parkcy1007@gmail.com>
2023-12-14 21:46:24 +0900

    init project

---
+++ src/main/java/com/docker/dockerpilot/DockerPilotApplication.java
@@ -0,0 +1,13 @@
+package com.docker.dockerpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docker/dockerpilot/DockerPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.dockerpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
