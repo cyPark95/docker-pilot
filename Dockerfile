424b55c307112713025fae90ec9da4a68f30a3db
cyPark95 <parkcy1007@gmail.com>
2023-12-26 15:29:43 +0900

    추가 설정

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM openjdk:17-alpine
-ARG JAR_FILE=/build/libs/docker-pilot-0.0.1-SNAPSHOT.jar
+FROM openjdk:17-ea-11-jdk-slim
+ARG JAR_FILE=/build/libs/*.jar
 COPY ${JAR_FILE} /app.jar
 ENTRYPOINT [ \
   "java", \
-#  "-Dspring.profiles.active=${PROFILE}",\
+  "-Dspring.profiles.active=${PROFILE}",\
   "-jar", \
   "app.jar" \
 ]
--- src/main/java/com/docker/dockerpilot/HelloController.java
+++ src/main/java/com/docker/dockerpilot/HelloController.java
@@ -1,11 +1,15 @@
 package com.docker.dockerpilot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${name}")
+    private String name;
 
     @GetMapping("/hello")
     String hello(@RequestParam(required = false) String name) {
